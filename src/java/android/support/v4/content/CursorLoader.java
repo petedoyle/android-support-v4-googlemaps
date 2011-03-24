427a05e1c86495402427c4da4e20ea97d32e4705
Pete Doyle <petedoyle@gmail.com>
2011-03-24 00:49:38 -0700

    Set the notification uri or the ContentObserver#onChange(boolean) is never called.

--- src/java/android/support/v4/content/CursorLoader.java
+++ src/java/android/support/v4/content/CursorLoader.java
@@ -61,6 +61,7 @@ public class CursorLoader extends AsyncTaskLoader<Cursor> {
      * when the cursor needs to be refreshed.
      */
     void registerContentObserver(Cursor cursor, ContentObserver observer) {
+        cursor.setNotificationUri( getContext().getContentResolver(), mUri );
         cursor.registerContentObserver(mObserver);
     }
 
